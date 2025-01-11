7ac4e83d207db590b6082495555f4706ba1f92d7
Franc <107664564+FrancGDev@users.noreply.github.com>
2025-01-10 21:34:08 -0300

    login fix

--- app/login/page.jsx
+++ app/login/page.jsx
@@ -29,7 +29,7 @@ export default function LoginPage() {
     };
 
     return (
-        <div className="flex items-center justify-center min-h-screen bg-gray-100">
+        <div className="flex items-center justify-center">
             <div className="bg-white p-8 rounded-lg shadow-lg w-full max-w-md">
                 <h1 className="text-2xl font-bold mb-6 text-center">Iniciar Sesión</h1>
                 {error && (
--- app/page.jsx
+++ app/page.jsx
@@ -3,9 +3,11 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <main>
-      <LoginPage />
-      <Link href="/register">registrarme</Link>
+    <main className="flex flex-col justify-center h-screen">
+      <LoginPage className="w-full h-full flex" />
+      <Link href="/register" className="my-4 text-blue-500 self-center">
+        Registrarme
+      </Link>
     </main>
   );
 }
